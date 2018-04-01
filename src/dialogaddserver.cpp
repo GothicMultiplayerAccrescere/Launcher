40c194303499103f042c0fcd87a96845ebbe424e
Christian Müller <cm.pgp@silicon-raccoon.com>
2018-04-01 19:21:24 +0200

    [*] fixed warning

--- src/dialogaddserver.cpp
+++ src/dialogaddserver.cpp
@@ -13,7 +13,7 @@ DialogAddServer::DialogAddServer(QWidget *parent) :
 
     connect(m_pUi->buttonAddServer, &QPushButton::clicked, [this]()
     {
-        emit selected(m_pUi->editServerName->text(), m_pUi->editUrl->text(), m_pUi->editPort->value());
+        emit selected(m_pUi->editServerName->text(), m_pUi->editUrl->text(), static_cast<quint16>(m_pUi->editPort->value()));
         close();
     });
 
