00330a66c26841245ba83f5f9452bfe45ddf4853
Patrick Kurras <VisualBasic@hotmail.de>
2018-04-05 22:15:41 +0200

    URL Check
    [*] changed url check in launcher to show the status
    [*] using new qt methods for regex now
    [-] regex only supports ipv4 now but ipv6 can be added later

--- src/dialogaddserver.cpp
+++ src/dialogaddserver.cpp
@@ -6,61 +6,64 @@
 #include "dialogaddserver.h"
 #include "ui_dialogaddserver.h"
 
-QRegExp DialogAddServer::ipV4Regex          = QRegExp("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$");
-QRegExp DialogAddServer::ipV6Regex          = QRegExp("^([0-9a-f]){1,4}(:([0-9a-f]){1,4}){7}$");
-QRegExp DialogAddServer::ipHostnameRegex    = QRegExp("^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])$");
+const char* invalidStyle = "background-color: #B22222; color: white;";
+// FIXME: incorporate hostname and ipv6 later if you want
+const QRegularExpression url = QRegularExpression("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
 
 DialogAddServer::DialogAddServer(QWidget *parent) :
     QDialog(parent),
-    m_pUi(new Ui::DialogAddServer),
-    m_IPValidator(ipV4Regex, this)
-{
+    m_pUi(new Ui::DialogAddServer) {
+
     m_pUi->setupUi(this);
-    m_pUi->editUrl->setValidator(&m_IPValidator);
 
-    connect(m_pUi->buttonAddServer, &QPushButton::clicked, [this]()
-    {
-        emit selected(m_pUi->editServerName->text(), m_pUi->editUrl->text(), static_cast<quint16>(m_pUi->editPort->value()));
-        close();
-    });
+	m_pUi->editUrl->setStyleSheet(invalidStyle);
 
-    connect(m_pUi->buttonCancel, &QPushButton::clicked, [this]()
-    {
+	connect(m_pUi->editUrl, &QLineEdit::editingFinished, [this]() {
+
+		QLineEdit* addressLine = this->m_pUi->editUrl;
+
+		// check url for regex match and set validity
+		if (!url.match(addressLine->text()).hasMatch())
+		{
+			addressLine->setStyleSheet(invalidStyle);
+			return;
+		}
+
+		this->hasValidInput = true;
+
+	});
+	connect(m_pUi->editUrl, &QLineEdit::textChanged, [this]() {
+
+		// we start changing the text: reset style and validity
+		QLineEdit* addressLine = this->m_pUi->editUrl;
+		addressLine->setStyleSheet(0);
+		this->hasValidInput = false;
+
+	});
+    connect(m_pUi->buttonCancel, &QPushButton::clicked, [this]() {
+
         close();
+
     });
+	connect(m_pUi->buttonAddServer, &QPushButton::clicked, [this]() {
+
+		QLineEdit* serverNameLine = this->m_pUi->editServerName;
+		QLineEdit* addressLine = this->m_pUi->editUrl;
+		quint16 port = this->m_pUi->editPort->value();
+
+		// check validation again and send the data to the main form
+		if (addressLine->text().size() > 0 && this->hasValidInput)
+		{
+			emit selected(serverNameLine->text(), addressLine->text(), port);
+		}
+
+		close();
+
+	});
 }
 
-DialogAddServer::~DialogAddServer()
-{
+DialogAddServer::~DialogAddServer() {
+
     delete m_pUi;
-}
 
-void DialogAddServer::checkRegexAndClearIfInvalid(QRegExp &regex)
-{
-    if(!regex.exactMatch(m_pUi->editUrl->text()))
-        m_pUi->editUrl->clear();
-}
-
-void DialogAddServer::on_rbt_IPv4_clicked()
-{
-    m_pUi->rbt_IPv6->setChecked(false);
-    m_pUi->rbt_IPHostname->setChecked(false);
-    m_IPValidator.setRegExp(ipV4Regex);
-    checkRegexAndClearIfInvalid(ipV4Regex);
-}
-
-void DialogAddServer::on_rbt_IPv6_clicked()
-{
-    m_pUi->rbt_IPv4->setChecked(false);
-    m_pUi->rbt_IPHostname->setChecked(false);
-    m_IPValidator.setRegExp(ipV6Regex);
-    checkRegexAndClearIfInvalid(ipV6Regex);
-}
-
-void DialogAddServer::on_rbt_IPHostname_clicked()
-{
-    m_pUi->rbt_IPv4->setChecked(false);
-    m_pUi->rbt_IPv6->setChecked(false);
-    m_IPValidator.setRegExp(ipHostnameRegex);
-    checkRegexAndClearIfInvalid(ipHostnameRegex);
 }
--- src/dialogaddserver.h
+++ src/dialogaddserver.h
@@ -16,18 +16,11 @@ public:
     virtual ~DialogAddServer() override;
 
 private:
+
     Ui::DialogAddServer *m_pUi;
-    QRegExpValidator m_IPValidator;
-    static QRegExp ipV4Regex;
-    static QRegExp ipV6Regex;
-    static QRegExp ipHostnameRegex;
-    void checkRegexAndClearIfInvalid(QRegExp &validator);
+	bool hasValidInput;
 
 signals:
     void selected(const QString &name, const QString &url, quint16 port);
 
-private slots:
-    void on_rbt_IPv4_clicked();
-    void on_rbt_IPv6_clicked();
-    void on_rbt_IPHostname_clicked();
 };
